5144986300e1efa2ee24f85ecbd377ea6bbafd3e
Ramona Fuchs <65505215+raexune@users.noreply.github.com>
2022-04-17 15:04:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Get user email adress
+print("What is your E-Mail adress? ")
+email = input()
+#slice out user name
+user = email[:email.index("@")]
+#slice domain name
+domain = email[email.index("@") + 1:]
+#format message
+output = f"Your username is {user} and your domainname is {domain}."
+#display output message
+print(output)
